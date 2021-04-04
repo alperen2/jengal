9589b9246b6f2cb2734b7d326ce1ba29716b7c73
alperen2 <alperen@lityum.com>
2021-04-04 15:35:07 +0300

    Post model oluşturuldu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Job Postings</h1>
+            <h1 class="m-0">My Postings</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -25,15 +25,55 @@
           <div class="col-lg-12">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Wordpress Tema düzenleme - <small>250$ - 400$</small></h5>
 
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
+                  Mevcut wordpress sitemizin temasında düzenlemeler yapmak istiyoruz. Wordpress konusunda tecrübeli arkadaşlar olursa sevinirim. 
                 </p>
 
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="#" class="card-link">Detay</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Wordpress Tema düzenleme - <small>250$ - 400$</small></h5>
+
+                <p class="card-text">
+                  Mevcut wordpress sitemizin temasında düzenlemeler yapmak istiyoruz. Wordpress konusunda tecrübeli arkadaşlar olursa sevinirim. 
+                </p>
+
+                <a href="#" class="card-link">Detay</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Wordpress Tema düzenleme - <small>250$ - 400$</small></h5>
+
+                <p class="card-text">
+                  Mevcut wordpress sitemizin temasında düzenlemeler yapmak istiyoruz. Wordpress konusunda tecrübeli arkadaşlar olursa sevinirim. 
+                </p>
+
+                <a href="#" class="card-link">Detay</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Wordpress Tema düzenleme - <small>250$ - 400$</small></h5>
+
+                <p class="card-text">
+                  Mevcut wordpress sitemizin temasında düzenlemeler yapmak istiyoruz. Wordpress konusunda tecrübeli arkadaşlar olursa sevinirim. 
+                </p>
+
+                <a href="#" class="card-link">Detay</a>
               </div>
             </div>
 
